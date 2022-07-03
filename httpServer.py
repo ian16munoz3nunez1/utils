94b406ee8e5b21bfb5d085bcbbaa41efcfbc2091
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-07-03 01:52:39 -0500

    2022-07-03_01:52 --> Cambios y mejoras

--- httpServer.py
+++ httpServer.py
@@ -11,23 +11,141 @@ def getNombre(ubicacion):
     nombre = os.path.basename(nombre)
     return nombre
 
+def getResponseFile(ubicacion):
+    href = ubicacion.replace(' ', '%20')
+    if isImage(ubicacion):
+        return f"<img src=\"{ubicacion}\" width=\"50%\" style=\"border-radius: 20%\"></img> <hr>"
+    elif isVideo(ubicacion):
+        return f"<video src=\"{ubicacion}\" width=\"50%\" controls loop></video> <hr>"
+    elif isAudio(ubicacion):
+        return f"<audio src=\"{ubicacion}\" width=\"50%\" controls></audio> <hr>"
+
+    elif ubicacion.endswith(".ino"):
+        return f"<a href={href}> <img src=\".httpServer/arduino.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b>  <hr>"
+    elif ubicacion.endswith(".c"):
+        return f"<a href={href}> <img src=\".httpServer/c.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".cpp"):
+        return f"<a href={href}> <img src=\".httpServer/cpp.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".css"):
+        return f"<a href={href}> <img src=\".httpServer/css.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif os.path.isdir(f"{directorio}/{ubicacion}"):
+        return f"<img src=\".httpServer/dir.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".ods") or ubicacion.endswith(".xlsx"):
+        return f"<a href={href}> <img src=\".httpServer/excel.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".xcf"):
+        return f"<a href={href}> <img src=\".httpServer/gimp.webp\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".go"):
+        return f"<a href={href}> <img src=\".httpServer/golang.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".html"):
+        return f"<a href={href}> <img src=\".httpServer/html.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".java") or ubicacion.endswith(".class") or ubicacion.endswith(".jar"):
+        return f"<a href={href}> <img src=\".httpServer/java.jpg\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".js"):
+        return f"<a href={href}> <img src=\".httpServer/js.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".lat"):
+        return f"<a href={href}> <img src=\".httpServer/latino.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".m"):
+        return f"<a href={href}> <img src=\".httpServer/octave.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".pdf"):
+        return f"<a href={href}> <img src=\".httpServer/pdf.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".pl"):
+        return f"<a href={href}> <img src=\".httpServer/perl.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".php"):
+        return f"<a href={href}> <img src=\".httpServer/php.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".odp") or ubicacion.endswith(".pptx"):
+        return f"<a href={href}> <img src=\".httpServer/powerpoint.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".p"):
+        return f"<a href={href}> <img src=\".httpServer/prolog.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".py"):
+        return f"<a href={href}> <img src=\".httpServer/python.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".rb"):
+        return f"<a href={href}> <img src=\".httpServer/ruby.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".sql"):
+        return f"<a href={href}> <img src=\".httpServer/sql.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".txt") or ubicacion.endswith(".dat"):
+        return f"<a href={href}> <img src=\".httpServer/txt.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    elif ubicacion.endswith(".odt") or ubicacion.endswith(".docx"):
+        return f"<a href={href}> <img src=\".httpServer/word.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+    else:
+        return f"<a href={href}> <b>{ubicacion}</b> </a> <hr>"
+
+def isImage(ubicacion):
+    ext = [".jpg", ".png", ".jpeg", ".webp", ".gif"]
+    extUpper = [i.upper() for i in ext]
+
+    imagen = False
+    i = 0
+    while i < len(ext):
+        if ubicacion.endswith(ext[i]) or ubicacion.endswith(extUpper[i]):
+            imagen = True
+            break
+        i += 1
+    return imagen
+
+def isVideo(ubicacion):
+    ext = [".mp4", ".avi", ".mkv", ".webm"]
+    extUpper = [i.upper() for i in ext]
+
+    video = False
+    i = 0
+    while i < len(ext):
+        if ubicacion.endswith(ext[i]) or ubicacion.endswith(extUpper[i]):
+            video = True
+            break
+        i += 1
+    return video
+
+def isAudio(ubicacion):
+    ext = [".mp3", ".mpga", ".wav", ".opus"]
+    extUpper = [i.upper() for i in ext]
+
+    audio = False
+    i = 0
+    while i < len(ext):
+        if ubicacion.endswith(ext[i]) or ubicacion.endswith(extUpper[i]):
+            audio = True
+            break
+        i += 1
+    return audio
+
 puerto = int(sys.argv[1])
 if len(sys.argv) == 2:
     directorio = os.getcwd()
-else:
-    directorio = os.getcwd() + '/' + sys.argv[2]
+elif len(sys.argv) == 3:
+    directorio = os.getcwd()
+    show = int(sys.argv[2])
+elif len(sys.argv) == 4:
+    show = int(sys.argv[2])
+    directorio = os.getcwd() + '/' + sys.argv[3]
+
 
 if os.path.isdir(directorio):
-    app = Flask(__name__)
+    app = Flask(__name__,
+                static_url_path='',
+                static_folder='.')
 
     @app.route("/")
     def principal():
         nombre = getNombre(directorio)
-        response = f"<h1> {nombre} </h1> <hr> <ul>"
-        for i in os.listdir(directorio):
-            href = i.replace(' ', "%20")
-            response += f"<li> <a href={href}> {i} </a> </li>"
-        response += "</ul>"
+        tam = len(os.listdir(directorio))
+        response = f"<title> {nombre} - {tam} elementos </title>"
+        response += f"<h1> {nombre} - {tam} elementos </h1> <hr>"
+        if show == 1:
+            response += "<div align=\"center\">"
+            for i in os.listdir(directorio):
+                if show == 1:
+                    response += getResponseFile(i)
+            response += "</div>"
+
+        else:
+            response += f"<ul>"
+            for i in os.listdir(directorio):
+                href = i.replace(' ', "%20")
+                if os.path.isdir(f"{directorio}/{i}"):
+                    response += f"<li> <a href={href}> {i}/ </a> </li>"
+                else:
+                    response += f"<li> <a href={href}> {i} </a> </li>"
+            response += "</ul>"
 
         return response
 
--- wget.py
+++ wget.py
@@ -1,35 +1,71 @@
 import requests
 import re
 import sys
+from urllib import request
+from bs4 import BeautifulSoup
 from colorama import init
 from colorama.ansi import Fore
 
 init(autoreset=True)
 
-extensiones = ["jpg", "png", "jpeg", "svg", "webp", "mp4", "avi", "mkv",
-    "html", "css", "txt", "dat", "py", "java", "c", "cpp", "js", "ino",
-    "go", "rb", "m", "h", "pdf", "docx", "pptx", "xlsx", "odt", "ods", "odp",
-    "odg", "xcf", "gif"]
-upperExtensiones = [i.upper() for i in extensiones]
-
-url = sys.argv[1]
-
-valido = False
-i = 0
-while i < len(extensiones):
-    if re.search(f"[.]{extensiones[i]}", url):
-        valido = True
-        extension = extensiones[i]
-        break
-    if re.search(f"[.]{upperExtensiones[i]}", url):
-        valido = True
-        extension = upperExtensiones[i]
-        break
-    i += 1
-
-if valido:
+def wgetNombre(url, extension):
     nombre = re.findall(f"/([a-zA-Z0-9_ ].+[.]{extension})", url)[0]
     nombre = nombre.split('/')[-1]
+    return nombre
+
+def getExt(url):
+    extensiones = ["jpg", "png", "jpeg", "svg", "webp", "mp4", "avi", "mkv",
+        "html", "css", "txt", "dat", "py", "java", "c", "cpp", "js", "ino",
+        "go", "rb", "m", "h", "pdf", "docx", "pptx", "xlsx", "odt", "ods", "odp",
+        "odg", "xcf", "gif", "webm", "mp3", "opus", "mpga", "wav"]
+    upperExtensiones = [i.upper() for i in extensiones]
+
+    valido = False
+    extension = None
+    i = 0
+    while i < len(extensiones):
+        if re.search(f"[.]{extensiones[i]}", url):
+            valido = True
+            extension = extensiones[i]
+            break
+        if re.search(f"[.]{upperExtensiones[i]}", url):
+            valido = True
+            extension = upperExtensiones[i]
+            break
+        i += 1
+
+    return valido, extension
+
+def getArchivos(soup, ext):
+    if ext == '*':
+        archivos = []
+        img = soup.find_all("img")
+        video = soup.find_all("video")
+        audio = soup.find_all("audio")
+        a = soup.find_all("a")
+        for i in img:
+            archivos.append(i)
+        for i in video:
+            archivos.append(i)
+        for i in audio:
+            archivos.append(audio)
+        for i in a:
+            archivos.append(i)
+    else:
+        archivos = soup.find_all(ext)
+
+    return archivos
+
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+elif len(sys.argv) == 3:
+    url = sys.argv[1]
+    ext = sys.argv[2]
+
+valido, extension = getExt(url)
+
+if valido and len(sys.argv) == 2 and extension is not None:
+    nombre = wgetNombre(url, extension)
 
     req = requests.get(url)
 
@@ -38,5 +74,46 @@ if valido:
     archivo.close()
     print(Fore.GREEN + f"[+] Archivo \"{nombre}\" creado correctamente")
 
+elif len(sys.argv) == 3 and not valido and extension is None:
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+
+    archivos = getArchivos(soup, ext)
+    for i in archivos:
+        try:
+            if ext == 'a':
+                link = i.get("href")
+            else:
+                link = i.get("src")
+            contenido = requests.get(link)
+        except:
+            if ext == 'a':
+                link = url + i.get("href")
+            else:
+                link = url + i.get("src")
+            contenido = requests.get(link)
+
+        extension = getExt(link)[1]
+        if extension is not None:
+            nombre = wgetNombre(link, extension)
+            with open(nombre, 'wb') as archivo:
+                archivo.write(contenido.content)
+            archivo.close()
+            print(Fore.GREEN + f"[+] Archivo \"{nombre}\" creado")
+
+elif extension == None and not valido and len(sys.argv) < 3:
+    print(Fore.YELLOW +  "[!] warning")
+    try:
+        req = request.urlopen(url)
+
+        nombre = input("Ingresa el nombre del archivo: ")
+        with open(nombre, 'wb') as archivo:
+            archivo.write(req.read())
+        archivo.close()
+        print(Fore.GREEN + f"[+] Archivo \"{nombre}\" creado")
+
+    except Exception as e:
+        print(e)
+
 else:
-    print(Fore.RED +  "error")
+    print(Fore.RED + "[-] error")

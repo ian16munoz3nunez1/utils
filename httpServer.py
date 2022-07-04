b61d5c0f1f38a1d54ee7b639a32ef6b645e29358
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-07-04 02:44:23 -0500

    2022-07-04_02:44 --> Mejoras en servidor httpy

--- httpServer.py
+++ httpServer.py
@@ -14,60 +14,60 @@ def getNombre(ubicacion):
 def getResponseFile(ubicacion):
     href = ubicacion.replace(' ', '%20')
     if isImage(ubicacion):
-        return f"<img src=\"{ubicacion}\" width=\"50%\" style=\"border-radius: 20%\"></img> <hr>"
+        return f"<img src=\"{ubicacion}\"></img>"
     elif isVideo(ubicacion):
-        return f"<video src=\"{ubicacion}\" width=\"50%\" controls loop></video> <hr>"
+        return f"<video src=\"{ubicacion}\" preload=\"none\" controls loop></video>"
     elif isAudio(ubicacion):
-        return f"<audio src=\"{ubicacion}\" width=\"50%\" controls></audio> <hr>"
+        return f"<audio src=\"{ubicacion}\" controls></audio>"
 
     elif ubicacion.endswith(".ino"):
-        return f"<a href={href}> <img src=\".httpServer/arduino.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b>  <hr>"
+        return f"<a href={href}> <img src=\".httpServer/arduino.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".c"):
-        return f"<a href={href}> <img src=\".httpServer/c.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/c.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".cpp"):
-        return f"<a href={href}> <img src=\".httpServer/cpp.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/cpp.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".css"):
-        return f"<a href={href}> <img src=\".httpServer/css.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/css.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif os.path.isdir(f"{directorio}/{ubicacion}"):
-        return f"<img src=\".httpServer/dir.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> <br> <b>{ubicacion}</b> <hr>"
+        return f"<img src=\".httpServer/dir.png\"></img> <br> <b>{ubicacion}</b>"
     elif ubicacion.endswith(".ods") or ubicacion.endswith(".xlsx"):
-        return f"<a href={href}> <img src=\".httpServer/excel.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/excel.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".xcf"):
-        return f"<a href={href}> <img src=\".httpServer/gimp.webp\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/gimp.webp\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".go"):
-        return f"<a href={href}> <img src=\".httpServer/golang.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/golang.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".html"):
-        return f"<a href={href}> <img src=\".httpServer/html.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/html.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".java") or ubicacion.endswith(".class") or ubicacion.endswith(".jar"):
-        return f"<a href={href}> <img src=\".httpServer/java.jpg\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/java.jpg\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".js"):
-        return f"<a href={href}> <img src=\".httpServer/js.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/js.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".lat"):
-        return f"<a href={href}> <img src=\".httpServer/latino.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/latino.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".m"):
-        return f"<a href={href}> <img src=\".httpServer/octave.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/octave.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".pdf"):
-        return f"<a href={href}> <img src=\".httpServer/pdf.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/pdf.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".pl"):
-        return f"<a href={href}> <img src=\".httpServer/perl.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/perl.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".php"):
-        return f"<a href={href}> <img src=\".httpServer/php.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/php.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".odp") or ubicacion.endswith(".pptx"):
-        return f"<a href={href}> <img src=\".httpServer/powerpoint.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/powerpoint.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".p"):
-        return f"<a href={href}> <img src=\".httpServer/prolog.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/prolog.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".py"):
-        return f"<a href={href}> <img src=\".httpServer/python.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/python.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".rb"):
-        return f"<a href={href}> <img src=\".httpServer/ruby.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/ruby.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".sql"):
-        return f"<a href={href}> <img src=\".httpServer/sql.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/sql.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".txt") or ubicacion.endswith(".dat"):
-        return f"<a href={href}> <img src=\".httpServer/txt.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/txt.png\"></img> <br> <b>{ubicacion}</b> </a>"
     elif ubicacion.endswith(".odt") or ubicacion.endswith(".docx"):
-        return f"<a href={href}> <img src=\".httpServer/word.png\" width=\"30%\" style=\"border-radius: 20%;\"></img> </a> <br> <b>{ubicacion}</b> <hr>"
+        return f"<a href={href}> <img src=\".httpServer/word.png\"></img> <br> <b>{ubicacion}</b> </a>"
     else:
-        return f"<a href={href}> <b>{ubicacion}</b> </a> <hr>"
+        return f"<a href={href}> <b>{ubicacion}</b> </a>"
 
 def isImage(ubicacion):
     ext = [".jpg", ".png", ".jpeg", ".webp", ".gif"]
@@ -128,14 +128,84 @@ if os.path.isdir(directorio):
     def principal():
         nombre = getNombre(directorio)
         tam = len(os.listdir(directorio))
-        response = f"<title> {nombre} - {tam} elementos </title>"
+        response = "<head>"
+        response += f"<title> {nombre} - {tam} elementos </title>"
+        response += f"<meta charset=\"utf-8\">"
+        response += """<style>
+            * {
+                box-sizing: border-box;
+                margin: 0;
+                padding: 0;
+            }
+
+            body {
+                background-color: rgb(100, 100, 100);
+            }
+
+            .galeria {
+                display: grid;
+                grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+                width: 95%;
+                margin: auto;
+                row-gap: 10px;
+                column-gap: 10px;
+                padding: 40px 0;
+                overflow: hidden;
+            }
+
+            .galeria img {
+                border-radius: 10%;
+                width: 100%;
+                vertical-align: top;
+                height: 300px;
+                object-fit: cover;
+                display: block;
+                position: relative;
+                overflow: hidden;
+            }
+
+            .galeria video {
+                border-radius: 10%;
+                width: 100%;
+                vertical-align: top;
+                height: 300px;
+                object-fit: cover;
+                display: block;
+                position: relative;
+                overflow: hidden;
+            }
+
+            .galeria audio {
+                border-radius: 10%;
+                width: 100%;
+                vertical-align: top;
+                height: 300px;
+                object-fit: cover;
+                display: block;
+                position: relative;
+                overflow: hidden;
+            }
+
+
+            .galeria a img {
+                border-radius: 10%;
+                width: 100%;
+                vertical-align: top;
+                height: 300px;
+                object-fit: cover;
+                display: block;
+                position: relative;
+                overflow: hidden;
+            }
+        </style>"""
+        response += "</head>"
         response += f"<h1> {nombre} - {tam} elementos </h1> <hr>"
         if show == 1:
-            response += "<div align=\"center\">"
+            response += "<section class=\"galeria\">"
             for i in os.listdir(directorio):
                 if show == 1:
                     response += getResponseFile(i)
-            response += "</div>"
+            response += "</section>"
 
         else:
             response += f"<ul>"

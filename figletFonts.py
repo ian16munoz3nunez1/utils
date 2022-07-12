23fc60331ffe7f4b39ec237cee7eb3464d3e72cb
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-07-12 17:31:21 -0500

    2022-07-12_17:31 --> figletFonts

---
+++ figletFonts.py
@@ -0,0 +1,21 @@
+import os
+import getpass
+import platform
+
+usuario = getpass.getuser()
+
+myOs = platform.system().lower()
+
+if myOs == "linux":
+    directorio = f"/home/{usuario}/figlet-fonts/"
+elif myOs == "windows":
+    directorio = f"C:\\users\\{usuario}\\figlet-fonts"
+
+fonts = os.listdir(directorio)
+fonts.sort()
+for i in fonts:
+    archivo = f"{directorio}/{i}"
+    if os.path.isfile(archivo) and i.endswith(".flf"):
+        texto = i[:i.index(".flf")]
+        os.system(f"figlet \"{texto}\" -f \"{i}\" -d {directorio}")
+

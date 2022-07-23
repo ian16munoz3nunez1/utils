ca042b38ba7a43d6e253c844fe8bbd69dde414d5
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-07-22 21:30:17 -0500

    2022-07-22_21:30 --> 'encrypt' y 'decrypt' actualizados

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,17 @@
 import os
 import sys
+import re
 from cryptography.fernet import Fernet
 from colorama import init
 from colorama.ansi import Fore
 
 init(autoreset=True)
 
+# 'cargarClave' carga la llave del archivo de encriptacion
 def cargarClave(clave):
     return open(clave, 'rb').read()
 
+# 'desencriptar' desencripta el arreglo de archivos 'archivos' con la llave 'key'
 def desencriptar(archivos, key):
     f = Fernet(key)
 
@@ -27,35 +30,74 @@ def desencriptar(archivos, key):
         print(Fore.GREEN + f"[+] Archivo \"{nombre}\" desencriptado")
 
 try:
-    if len(sys.argv) == 1:
-        print(Fore.RED + "Error de sintaxis")
+    # Se obtiene el comando ingresado
+    cmd = ''.join(' ' + i for i in sys.argv)
 
-    else:
-        clave = sys.argv[1]
-        directorio = sys.argv[2]
+    # Se revisa si los parametros y la sintaxis son correctos
+    if re.search("-k[= ]", cmd) and re.search("-p[= ]", cmd):
+        mKey = re.search("-k[= ]", cmd)
+        mUbicacion = re.search("-p[= ]", cmd)
 
-        if os.path.isdir(directorio):
+        # Se obtienen los parametros del comando
+        clave = ''
+        ubicacion = ''
+        if mKey.start() < mUbicacion.start():
+            clave = cmd[mKey.end():mUbicacion.start()-1]
+            ubicacion = cmd[mUbicacion.end():]
+        if mUbicacion.start() < mKey.start():
+            ubicacion = cmd[mUbicacion.end():mKey.start()-1]
+            clave = cmd[mKey.end():]
+
+        # Si la ubicacion ingresada es un archivo...
+        if os.path.isfile(ubicacion) and clave.endswith(".key"):
             if os.path.isfile(clave):
-                print(Fore.YELLOW + f"[!] Segur@ que quieres usar la clave \"{clave}\"?...\n[S/n]", end='')
-                res = input()
+                print(Fore.YELLOW + f"[!] Segur@ que quieres desencriptar el archivo \"{ubicacion}\"?...\n[S/n]", end=' ')
+                res = input() # Se espera una respuesta
 
-                if len(res) == 0 or res.upper() == 'S':
-                    key = cargarClave(clave)
+                if len(res) == 0 or res.upper() == 'S' or res.upper()[0] == 'S':
+                    key = cargarClave(clave) # Se carga la llave
+                    desencriptar([ubicacion], key) # Se encripta el archivo
 
-                    archivos = []
-                    for i in os.listdir(directorio):
-                        archivo = f"{directorio}/{i}"
-                        if os.path.isfile(archivo):
-                            archivos.append(archivo)
-
-                    desencriptar(archivos, key)
-
-                    os.remove(clave)
+                    os.remove(clave) # Se elimina la llave ingresada
                     print(Fore.YELLOW + f"[!] Clave \"{clave}\" eliminada")
                 else:
-                    print(Fore.YELLOW + "Desencriptacion cancelada")
+                    print(Fore.YELLOW + "[!] Desencriptacion cancelada")
+
+            else:
+                print(Fore.RED + f"[-] Error al encontrar la clave \"{clave}\"")
+                print(Fore.YELLOW + "[!] Desencriptacion cancelada")
+
+        # Si la ubicacion ingresada es un directorio...
+        elif os.path.isdir(ubicacion) and clave.endswith(".key"):
+            if os.path.isfile(clave):
+                print(Fore.YELLOW + f"[!] Segur@ que quieres usar la clave \"{clave}\"?...\n[S/n]", end=' ')
+                res = input() # Se espera una respuesta
+
+                if len(res) == 0 or res.upper() == 'S' or res.upper()[0] == 'S':
+                    key = cargarClave(clave) # Se carga la llave
+
+                    # Se crea una lista de archivos
+                    archivos = []
+                    for i in os.listdir(ubicacion):
+                        archivo = f"{ubicacion}/{i}"
+                        if os.path.isfile(archivo) and not i.endswith(".key"):
+                            archivos.append(archivo)
+
+                    desencriptar(archivos, key) # Se desencriptan los archivos
+
+                    os.remove(clave) # Se elimina la llave ingresada
+                    print(Fore.YELLOW + f"[!] Clave \"{clave}\" eliminada")
+
+                else:
+                    print(Fore.YELLOW + "[!] Desencriptacion cancelada")
+            else:
+                print(Fore.RED + f"[-] Error al encontrar la clave \"{clave}\"")
+
         else:
-            print(Fore.RED + f"[-] Error al encontrar el directorio \"{directorio}\"")
+            print(Fore.RED + f"[-] Error al encontrar la ubicacion \"{ubicacion}\"")
+    else:
+        print(Fore.RED + "[-] Error de sintaxis")
 
 except:
     print(Fore.RED + "error")
+
--- encrypt.py
+++ encrypt.py
@@ -1,23 +1,31 @@
 import os
 import sys
+import re
 from cryptography.fernet import Fernet
 from colorama import init
 from colorama.ansi import Fore
 
 init(autoreset=True)
 
+# 'generarClave' crea un archivo .key en el que se almacena la llave de encriptacion
 def generarClave(clave):
-    key = Fernet.generate_key()
+    try:
+        key = Fernet.generate_key()
 
-    with open(clave, 'wb') as keyFile:
-        keyFile.write(key)
-    keyFile.close()
+        with open(clave, 'wb') as keyFile:
+            keyFile.write(key)
+        keyFile.close()
 
-    print(Fore.GREEN + f"[+] Clave \"{clave}\" generada")
+        print(Fore.GREEN + f"[+] Clave \"{clave}\" generada")
 
+    except:
+        pass
+
+# 'cargarClave' carga la llave del archivo de encriptacion
 def cargarClave(clave):
     return open(clave, 'rb').read()
 
+# 'encriptar' encripta todos los elementos del arreglo 'archivos' con la llave 'key'
 def encriptar(archivos, key):
     f = Fernet(key)
     for i in archivos:
@@ -35,29 +43,72 @@ def encriptar(archivos, key):
         print(Fore.GREEN + f"[+] Archivo \"{nombre}\" encriptado")
 
 try:
-    if len(sys.argv) == 1:
-        print(Fore.RED + "[-] Error de sintaxis")
+    # Se obtiene el comando ingresado
+    cmd = ''.join(' ' + i for i in sys.argv)
 
-    else:
-        clave = sys.argv[1]
-        directorio = sys.argv[2]
+    # Se revisa si los parametros y la sintaxis son correctos
+    if re.search("-k[= ]", cmd) and re.search("-p[= ]", cmd):
+        mKey = re.search("-k[= ]", cmd)
+        mUbicacion = re.search("-p[= ]", cmd)
 
-        if os.path.isdir(directorio):
-            generarClave(clave)
+        # Se obtienen los parametros
+        clave = ''
+        ubicacion = ''
+        if mKey.start() < mUbicacion.start():
+            clave = cmd[mKey.end():mUbicacion.start()-1]
+            ubicacion = cmd[mUbicacion.end():]
+        if mUbicacion.start() < mKey.start():
+            ubicacion = cmd[mUbicacion.end():mKey.start()-1]
+            clave = cmd[mKey.end():]
+
+        # Si la ubicacion ingresada es un archivo...
+        if os.path.isfile(ubicacion) and clave.endswith(".key"):
+            generarClave(clave) # Se genera una nueva llave
             if os.path.isfile(clave):
-                key = cargarClave(clave)
+                print(Fore.YELLOW + f"[!] Segur@ que quieres encriptar el archivo \"{ubicacion}\"?...\n[S/n]", end=' ')
+                res = input() # Se espera una respuesta
 
-                archivos = []
-                for i in os.listdir(directorio):
-                    archivo = f"{directorio}/{i}"
-                    if os.path.isfile(archivo):
-                        archivos.append(archivo)
-
-                encriptar(archivos, key)
+                if len(res) == 0 or res.upper() == 'S' or res.upper()[0] == 'S':
+                    key = cargarClave(clave) # Se carga la llave
+                    encriptar([ubicacion], key) # Se encripta el archivo
+                else:
+                    os.remove(clave) # Se elimina la ultima llave creada
+                    print(Fore.YELLOW + f"[!] Clave \"{clave}\" eliminada")
+                    print(Fore.YELLOW + "[!] Encriptacion cancelada")
             else:
                 print(Fore.RED + f"[-] Error al encontrar la clave \"{clave}\"")
+                print(Fore.YELLOW + "[!] Encriptacion cancelada")
+
+        # Si la ubicacion ingresada es un directorio
+        elif os.path.isdir(ubicacion) and clave.endswith(".key"):
+            # Se crea una lista de archivos
+            archivos = []
+            for i in os.listdir(ubicacion):
+                archivo = f"{ubicacion}/{i}"
+                if os.path.isfile(archivo):
+                    archivos.append(archivo)
+
+            generarClave(clave) # Se genera una nueva llave
+            if os.path.isfile(clave):
+                print(Fore.YELLOW + f"[!] Segur@ que quieres encriptar el directorio \"{ubicacion}\"?...\n[S/n]", end=' ')
+                res = input() # Se espera una respuesta
+
+                if len(res) == 0 or res.upper() == 'S' or res.upper()[0] == 'S':
+                    key = cargarClave(clave) # Se carga la llave
+                    encriptar(archivos, key) # Se encriptan los archivos
+                else:
+                    os.remove(clave) # Se elimina la ultima llave creada
+                    print(Fore.YELLOW + f"[!] Clave \"{clave}\" eliminada")
+                    print(Fore.YELLOW + f"[!] Encriptacion cancelada")
+
+            else:
+                print(Fore.RED + f"[-] Error al encontrar la clave \"{clave}\"")
+                print(Fore.YELLOW + "[!] Encriptacion cancelada")
 
         else:
-            print(Fore.RED + f"[-] Error al encontrar el directorio \"{directorio}\"")
+            print(Fore.RED + f"[-] Error al encontrar la ubicacion \"{ubicacion}\" o la llave \"{clave}\"")
+    else:
+        print(Fore.RED + "[-] Error de sintaxis")
+
 except:
     print(Fore.RED + "error")

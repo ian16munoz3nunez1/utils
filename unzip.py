e3c400f9a19d9fa45e7a9a5f837792bfc218ce72
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-07-22 23:30:55 -0500

    2022-07-22_23:30 --> 'zip' y 'unzip' actualizados

--- unzip.py
+++ unzip.py
@@ -1,26 +1,53 @@
 import os
 import sys
+import re
 from zipfile import ZipFile
 from colorama import init
 from colorama.ansi import Fore
 
 init(autoreset=True)
 
-archivo = sys.argv[1]
-directorio = sys.argv[2]
+# 'getNombre' regresa el nombre de un archivo o directorio
+def getNombre(ubicacion):
+    nombre = os.path.abspath(ubicacion)
+    nombre = os.path.basename(nombre)
+    return nombre
 
-if os.path.isfile(archivo):
-    if not os.path.isdir(directorio):
-        os.mkdir(directorio)
+cmd = ''.join(' ' + i for i in sys.argv) # Se obtiene el comando ingresado
 
-    cont = 0
-    with ZipFile(archivo, 'r') as zip:
+# Se revisa si la sintaxis y los parametros son correctos
+try:
+    mOrigen = re.search("-o[= ]", cmd)
+    mDestino = re.search("-d[= ]", cmd)
+
+    origen = ''
+    destino = ''
+    if mOrigen.start() < mDestino.start():
+        origen = cmd[mOrigen.end():mDestino.start()-1]
+        destino = cmd[mDestino.end():]
+    if mDestino.start() < mOrigen.start():
+        destino = cmd[mDestino.end():mOrigen.start()-1]
+        origen = cmd[mOrigen.end():]
+
+except:
+    print(Fore.RED + "[-] Error de sintaxis")
+    exit()
+
+# Si el origen es un archivo zip
+if os.path.isfile(origen) and (origen.endswith(".zip") or origen.endswith(".rar")):
+    # Si el directorio destino no existe se crea
+    if not os.path.isdir(destino):
+        os.mkdir(destino)
+
+    descomprimidos = 0
+    with ZipFile(origen, 'r') as zip: # Se crea la instancia del archivo 'origen'
         for i in zip.namelist():
-            zip.extract(i, directorio)
+            zip.extract(i, destino) # Se descomprime el archivo 'i' en 'destino'
             print(Fore.GREEN + f"[+] Archivo \"{i}\" descomprimido")
-            cont += 1
+            descomprimidos += 1
     zip.close()
 
-    print(f"[+] {cont} archivos descomprimidos")
+    print(f"[+] {descomprimidos} archivos descomprimidos")
+
 else:
-    print(Fore.RED + f"[-] Archivo \"{archivo}\" no encontrado")
+    print(Fore.RED + f"[-] Archivo \"{origen}\" no encontrado")
--- zip.py
+++ zip.py
@@ -1,26 +1,91 @@
 import os
 import sys
+import re
 from zipfile import ZipFile
 from colorama import init
 from colorama.ansi import Fore
 
 init(autoreset=True)
 
-directorio = sys.argv[1]
-archivo = sys.argv[2]
+# 'getNombre' regresa el nombre de un archivo o directorio
+def getNombre(ubicacion):
+    nombre = os.path.abspath(ubicacion)
+    nombre = os.path.basename(nombre)
+    return nombre
 
-if os.path.isdir(directorio):
-    cont = 0
-    with ZipFile(archivo, 'w') as zip:
-        for i in os.listdir(directorio):
-            path = f"{directorio}/{i}"
-            if os.path.isfile(path):
-                zip.write(path, i)
-                print(Fore.GREEN + f"[+] Archivo \"{i}\" comprimido")
-                cont += 1
-    zip.close()
+cmd = ''.join(' ' + i for i in sys.argv) # Se obtiene el comando ingresado
 
-    print(Fore.GREEN + f"[+] {cont} archivos comprimidos")
+# Se revisa si la sintaxis y los parametros son correctos
+try:
+    mOrigen = re.search("-o[= ]", cmd)
+    mDestino = re.search("-d[= ]", cmd)
 
+    origen = ''
+    destino = ''
+    if mOrigen.start() < mDestino.start():
+        origen = cmd[mOrigen.end():mDestino.start()-1]
+        destino = cmd[mDestino.end():]
+    if mDestino.start() < mOrigen.start():
+        destino = cmd[mDestino.end():mOrigen.start()-1]
+        origen = cmd[mOrigen.end():]
+except:
+    print(Fore.RED + "[-] Error de sintaxis")
+    exit()
+
+if re.search("-o[= ]", cmd) and re.search("-d[= ]", cmd):
+    # Si el origen es un archivo y el destino es un archivo zip...
+    if os.path.isfile(origen) and (destino.endswith(".zip") or destino.endswith(".rar")):
+        try:
+            # Se comprime el archivo
+            with ZipFile(destino, 'w') as zip:
+                zip.write(origen, getNombre(origen))
+            zip.close()
+            print(Fore.GREEN + f"[+] Archivo \"{origen}\" comprimido")
+
+        except:
+            print(Fore.RED + f"[-] Error al comprimir el archivo \"{origen}\"")
+
+    # Si el origen contiene '/', no es un directorio y el destino es un archivo zip...
+    elif re.search('/', origen) and not os.path.isdir(origen) and (destino.endswith(".zip") or destino.endswith(".rar")):
+        try:
+            archivos = origen.split('/') # Se crea un arreglo de archivos
+            directorio = os.getcwd() # Se obtiene el directorio actual
+            with ZipFile(destino, 'w') as zip:
+                for i in archivos:
+                    archivo = f"{directorio}/{i}" # Se obtiene el path del acrhivo
+                    if os.path.isfile(archivo):
+                        zip.write(archivo, getNombre(archivo)) # Se comprime el arhivo
+                        print(Fore.GREEN + f"[+] Archivo \"{i}\" comprimido")
+            zip.close()
+
+        except:
+            print(Fore.RED + f"[-] Error al comprimir los archivos")
+
+    elif os.path.isdir(origen) and (destino.endswith(".zip") or destino.endswith(".rar")):
+        try:
+            cont = len(os.listdir(origen)) # Se obtiene el numero de elementos del directorio
+            # Se crea una lista de archivos
+            archivos = []
+            for i in os.listdir(origen):
+                archivo = f"{origen}/{i}"
+                if os.path.isfile(archivo):
+                    archivos.append(archivo)
+
+            comprimidos = 0
+            with ZipFile(destino, 'w') as zip:
+                for i in archivos:
+                    zip.write(i, getNombre(i)) # Se comprime el archivo
+                    print(Fore.GREEN + f"[+] Archivo \"{i}\" comprimido")
+                    comprimidos += 1
+            zip.close()
+
+            print(Fore.GREEN + f"[+] {cont} elementos comprimidos de {cont}")
+
+        except:
+            print(Fore.RED + f"[-] Error al comprimir el directorio \"{origen}\"")
+
+    else:
+        print(Fore.RED + f"[-] Ubicacion \"{origen}\" no encontrada o problema con el parametro de destino")
 else:
-    print(Fore.RED + f"[-] Directorio \"{directorio}\" no encontrado")
+    print(Fore.RED + "Error de sintaxis")
+
